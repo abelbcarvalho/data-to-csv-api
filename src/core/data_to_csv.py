019989db5c574a729856ecbf0ab7445804616305
Abel Carvalho <160126794+abelbcarvalho@users.noreply.github.com>
2024-10-31 23:41:25 -0300

    feat: json to csv core and tests (#4)
    * feat: created a method to converts from json to csv
    * test: core data json to csv success
    
    * hotfix: test failed try to solve
    
    * hotfix: test failed try to solve two
    
    * hotfix: test failed try to solve three

---
+++ src/core/data_to_csv.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+class DataToCsv:
+    async def json_to_csv(self, data: list[dict]) -> any:
+        data_frame = pd.DataFrame(data)
+
+        csv = data_frame.to_csv(index=False)
+
+        return csv.replace("\r\n", "\n")
---
+++ tests/mocks/json_csv.py
@@ -0,0 +1,27 @@
+json_success = [
+    dict(
+        name="paula",
+        age=23,
+        gender="female",
+        occupation="scientist"
+    ),
+    dict(
+        name="maria",
+        age=25,
+        gender="female",
+        occupation="developer"
+    ),
+    dict(
+        name="joseph",
+        age=20,
+        gender="male",
+        occupation="lawyer"
+    )
+]
+
+csv_success = (
+    'name,age,gender,occupation\n'+
+    'paula,23,female,scientist\n' +
+    'maria,25,female,developer\n' +
+    'joseph,20,male,lawyer\n'
+)
