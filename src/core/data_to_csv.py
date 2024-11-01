059a3a05e03832c4803de917cec854fefc475968
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-11-01 13:16:46 -0300

    fix: type of data to core class

--- src/core/data_to_csv.py
+++ src/core/data_to_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 class DataToCsv:
-    async def json_to_csv(self, data: list[dict]) -> any:
+    async def json_to_csv(self, data: list[dict[str, any]]) -> any:
         data_frame = pd.DataFrame(data)
 
         csv = data_frame.to_csv(index=False)
