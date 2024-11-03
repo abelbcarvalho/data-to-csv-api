d71e87c06a25fbda2a0057aca4ed246f6a68c114
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-11-02 23:54:11 -0300

    feat: prototypes use name improved

--- src/prototypes/prototype_service_json_csv.py
+++ src/prototypes/prototype_service_json_csv.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 
-class InterServiceJsonCsv(ABC):
+class PrototypeServiceJsonCsv(ABC):
     @abstractmethod
     async def json_to_csv_swap(self, data: list[dict[str, any]]) -> str:
         pass
